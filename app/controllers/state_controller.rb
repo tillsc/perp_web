d57f1f2f3af486112dbbd8e39afd8a56808469c2
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-06-07 14:31:19 +0200

    Design status view

--- app/controllers/state_controller.rb
+++ app/controllers/state_controller.rb
@@ -2,8 +2,9 @@ class StateController < ApplicationController
 
   def index
     @status = {}
-    @status['MySQL Slave Status'] = ActiveRecord::Base.connection.exec_query('SHOW SLAVE STATUS').to_hash[0]
-    @status['MySQL Master Status'] = ActiveRecord::Base.connection.exec_query('SHOW MASTER STATUS').to_hash[0]
+    @status['MySQL Slave Status'] = ActiveRecord::Base.connection.exec_query('SHOW SLAVE STATUS').to_hash[0] || {}
+    @status['MySQL Slave Status'][:failure] = @status['MySQL Slave Status']['Slave_IO_Running'] == 'No'
+    @status['MySQL Master Status'] = ActiveRecord::Base.connection.exec_query('SHOW MASTER STATUS').to_hash[0] || {}
   end
 
 end
