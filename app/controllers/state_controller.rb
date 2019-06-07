190d499966cc0dfdd3a8bf9c79594131c22511cf
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-06-07 15:19:52 +0200

    Fix state coloring

--- app/controllers/state_controller.rb
+++ app/controllers/state_controller.rb
@@ -3,7 +3,8 @@ class StateController < ApplicationController
   def index
     @status = {}
     @status['MySQL Slave Status'] = ActiveRecord::Base.connection.exec_query('SHOW SLAVE STATUS').to_hash[0] || {}
-    @status['MySQL Slave Status'][:failure] = @status['MySQL Slave Status']['Slave_IO_Running'] == 'No'
+    io_state = @status['MySQL Slave Status']['Slave_IO_Running']
+    @status['MySQL Slave Status'][:failure] = io_state.present? && (io_state != 'Yes')
     @status['MySQL Master Status'] = ActiveRecord::Base.connection.exec_query('SHOW MASTER STATUS').to_hash[0] || {}
   end
 
