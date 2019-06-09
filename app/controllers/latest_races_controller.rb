78f4cdab422cc46048f5a1a56c9de9063b3d6526
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-06-09 13:49:34 +0200

    Fix empty overlay views

--- app/controllers/latest_races_controller.rb
+++ app/controllers/latest_races_controller.rb
@@ -6,7 +6,7 @@ class LatestRacesController < ApplicationController
     @race = scope.first
     max_measuring_point_number = @race && @race.results.map { |r| r.times.map(&:measuring_point_number).max }.compact.max
     @measuring_point = max_measuring_point_number && MeasuringPoint.find([@regatta.ID, max_measuring_point_number])
-    @results = @race.results.sort_by { |r| r.time_for(@measuring_point).try(:time) || 'ZZZZZZZZZ' }
+    @results = @race && @race.results.sort_by { |r| r.time_for(@measuring_point).try(:time) || 'ZZZZZZZZZ' }
     render :layout => 'minimal'
   end
 
@@ -15,7 +15,7 @@ class LatestRacesController < ApplicationController
     scope = Race.joins(results: :times).where('zeiten.MesspunktNr' => @measuring_point.number).for_regatta(@regatta).stated_minutes_ago(30)
     scope = scope.by_type_short(params[:type_short].to_s.split(',')) if params[:type_short].present?
     @race = scope.first
-    @result = @race && @race.results && @race.results.
+    @result = @race && @race.results.
         select { |r| r.time_for(@measuring_point).try(:time) }.
         sort_by { |r| r.time_for(@measuring_point).try(:time) }.
         first
