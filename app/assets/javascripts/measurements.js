3a5e58767d8e370f0e7da7aed46f84a5c4a9d943
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2021-06-06 14:45:48 +0200

    Extend view

--- app/assets/javascripts/measurements.js
+++ app/assets/javascripts/measurements.js
@@ -21,32 +21,42 @@ dragula([selected, selectable]).on('drag', function(el, source) {
   // your logic on dragend
 });
 
-
 var times = document.getElementById('times')
 
+function stopTime() {
+  var d = new Date();
+  var t = document.createElement('div')
+  t.classList.add('item_list__item')
+  t.innerHTML = '<span class="text-nowrap time">' + d.getHours() + ':' +
+    String(d.getMinutes()).padStart(2, '0')  + ':' +
+    String(d.getSeconds()).padStart(2, '0') + '.' +
+    String(Math.round(d.getMilliseconds() / 10)).padStart(2, '0') +
+    '</span>'
+  times.appendChild(t)
+
+  var delBtn =  document.createElement('a')
+  delBtn.classList.add('btn')
+  delBtn.classList.add('btn-danger')
+  delBtn.classList.add('btn-sm')
+  delBtn.classList.add('float-right')
+  delBtn.innerHTML = 'X'
+  delBtn.addEventListener("click", function() {
+    t.remove();
+  });
+  t.appendChild(delBtn);
+
+  return t;
+}
+
+
+document.getElementById('stop_time').addEventListener("click", function(e) {
+  stopTime();
+  e.preventDefault();
+  return false;
+})
 document.addEventListener("keydown", function(e) {
   if (!e.handled && e.key == 'Enter') {
-    var d = new Date();
-    var t = document.createElement('div')
-    t.classList.add('item_list__item')
-    t.innerHTML = '<span class="text-nowrap time">' + d.getHours() + ':' +
-      String(d.getMinutes()).padStart(2, '0')  + ':' +
-      String(d.getSeconds()).padStart(2, '0') + '.' +
-        String(Math.round(d.getMilliseconds() / 10)).padStart(2, '0') +
-        '</span>'
-    times.appendChild(t)
-
-    var delBtn =  document.createElement('a')
-    delBtn.classList.add('btn')
-    delBtn.classList.add('btn-danger')
-    delBtn.classList.add('btn-sm')
-    delBtn.classList.add('float-right')
-    delBtn.innerHTML = 'X'
-    delBtn.addEventListener("click", function() {
-      t.remove();
-    });
-
-    t.appendChild(delBtn)
+    stopTime();
 
     e.handled = true;
     return true;
