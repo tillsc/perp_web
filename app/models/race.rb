d8b9f43c1f857cce148014b20dff852ccd688795
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2022-06-04 14:55:28 +0200

    Allow races taking longer then 10 minutes

--- app/models/race.rb
+++ app/models/race.rb
@@ -56,7 +56,7 @@ class Race < ApplicationRecord
   end
 
   scope :now, -> do
-    where(arel_table['IstStartZeit'].between((10.minutes.ago)..(Time.current.getlocal))).
+    where(arel_table['IstStartZeit'].between((12.minutes.ago)..(Time.current.getlocal))).
         where('DATE(SollStartZeit) = ?', Date.today).
         order('DATE(SollStartZeit) DESC, IstStartZeit DESC')
   end
