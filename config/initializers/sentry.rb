f015c414063803697d929f89e02ce8ce514d3ab3
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2023-05-09 09:28:33 +0200

    Disable sentry in dev and test

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://f14590d7aa5841d08fdd54065ecf692e@o4504883820167168.ingest.sentry.io/4504883823640576'
+  config.enabled_environments = %w[production]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
