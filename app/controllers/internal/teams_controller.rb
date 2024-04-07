8565f48f54a9a9ded103afe2206a872da568af56
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2024-04-07 09:13:24 +0200

    Prevent n+1 query

--- app/controllers/internal/teams_controller.rb
+++ app/controllers/internal/teams_controller.rb
@@ -5,7 +5,7 @@ module Internal
     respond_to :json, :html
 
     def index
-      @teams = @regatta.teams
+      @teams = @regatta.teams.preload(:representative)
       @teams = @teams.by_filter(params[:query]) if params[:query].present?
       respond_with @teams
     end
