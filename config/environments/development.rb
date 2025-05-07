7e708e18d56711a4c659f1d55700dfe596155e64
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-05-07 22:13:21 +0200

    Raise unpermitted params in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
