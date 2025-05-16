4bee52a01fedeb7e0a6acddff83f9a5eeb1b2dfd
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-05-16 22:13:35 +0200

    Fix problems with representatives having no participants

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -7,7 +7,7 @@ class MyController < ApplicationController
       find_by!(public_private_id: params[:public_private_id])
     cookies[:my_public_private_id] = @address.public_private_id unless params[:no_cookie]
 
-    @participants = if @address.is_representative?
+    @participants = if @address.is_representative? && @address.teams.any?
                       @regatta.participants.where(team: @address.teams)
                     elsif @address.is_club?
                       @regatta.participants.for_club(@address)
