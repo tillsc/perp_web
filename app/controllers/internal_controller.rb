6e71088fe274fcb7106d23c64495b942b410ae9c
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-05-21 16:20:54 +0200

    Add more statistics

--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -7,11 +7,15 @@ class InternalController < ApplicationController
   end
 
   def statistics
-    @stats = @regatta.participants.
-      select("COUNT(*) as total_participants_count, SUM(Abgemeldet) as withdrawn_participants_count, SUM(Nachgemeldet) as late_participants_count").
+    @participants_stats = @regatta.participants.left_joins(:event).
+      select("COUNT(*) AS total_participants_count, SUM(Abgemeldet) AS withdrawn_participants_count, SUM(Nachgemeldet) AS late_participants_count").
       select("SUM(Meldegeld) AS entry_fee_sum").
       select("SUM((NOT Abgemeldet) * (rennen.RudererAnzahl + IF(rennen.MitSteuermann, 1, 0))) AS starting_rowers_count").
-      left_joins(:event).unscope(:order).first
+      unscope(:order).first
+    @teams_stats = @regatta.teams.where(
+      team_id: @regatta.participants.enabled.unscope(:order).select(:team_id)).
+      select("COUNT(*) AS total_teams_count").
+    unscope(:order).first
   end
 
 end
