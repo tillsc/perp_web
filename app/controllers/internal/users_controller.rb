16c9e4a21c8a084972c27884afd0bdeb004110d3
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-05-24 08:49:21 +0200

    Allow creating users too

--- app/controllers/internal/users_controller.rb
+++ app/controllers/internal/users_controller.rb
@@ -9,6 +9,25 @@ module Internal
       @users = User.all
     end
 
+    def new
+      authorize! :new, User
+
+      @user = User.new
+    end
+
+    def create
+      authorize! :create, User
+
+      @user = User.new(user_params)
+      if @user.save
+        flash[:info] = helpers.success_message_for(:create, @user)
+        redirect_to back_or_default_with_uri_params(dialog_finished_with: @user.id)
+      else
+        flash[:danger] = helpers.error_message_for(:create, @user)
+        render :new
+      end
+    end
+
     def edit
       @user = User.find(params[:id])
       authorize! :edit, @user
@@ -47,7 +66,11 @@ module Internal
     end
 
     def user_params
-      params.require(:user).permit(:email, :highlight_nobr, :password, :password_confirmation, *User::ALL_ROLES.map { |r| "role_#{r}".to_sym })
+      params.require(:user).
+        permit(:email, :password, :password_confirmation,
+               :confirmed_at, :highlight_nobr,
+               *User::ALL_ROLES.map { |r| "role_#{r}".to_sym }).
+        reject { |k, v| k.start_with?("password") && v.blank? }
     end
 
   end
