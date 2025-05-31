804b3987ddda766949701ca92b38707e37c07884
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-05-31 06:04:02 +0200

    Fix frozen Hash error

--- app/models/concerns/alias_attributes_in_json.rb
+++ app/models/concerns/alias_attributes_in_json.rb
@@ -1,6 +1,7 @@
 module AliasAttributesInJson
 
   def as_json(options = nil)
+    options = options.dup
     options[:methods]||= []
     options[:methods] += self.class.attribute_aliases.keys
     options[:except]||= []
