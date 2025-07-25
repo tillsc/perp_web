ca58ecc453271229a32f36a495d51bdd372494e2
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-07-25 10:49:22 +0200

    Move sentry settings to env

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,11 @@
-Sentry.init do |config|
-  config.dsn = 'https://f14590d7aa5841d08fdd54065ecf692e@o4504883820167168.ingest.sentry.io/4504883823640576'
-  config.enabled_environments = %w[production]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+if ENV["SENTRY_DSN"].present?
+  Sentry.init do |config|
+    config.dsn = ENV.fetch("SENTRY_DSN", nil)
+    config.enabled_environments = %w[production]
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  config.traces_sample_rate = 0.001
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    config.traces_sample_rate = 0.001
+  end
 end
