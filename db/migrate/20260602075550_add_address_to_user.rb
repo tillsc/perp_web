af2f82a9de3f9a09e1fd3ca39ea22985c6ca4e67
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2026-06-02 10:54:20 +0200

    Disable foreign key again...

--- db/migrate/20260602075550_add_address_to_user.rb
+++ db/migrate/20260602075550_add_address_to_user.rb
@@ -2,6 +2,5 @@ class AddAddressToUser < ActiveRecord::Migration[8.1]
   def change
     add_column :users, :address_id, :integer
     add_index :users, :address_id
-    add_foreign_key :users, :addressen, column: :address_id, primary_key: "ID"
   end
 end
